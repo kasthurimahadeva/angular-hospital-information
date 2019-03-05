4b688953af6f30162d364fae01790c56533485cf
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-03-05 23:33:52 +0530

    side-bar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [SideBarComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
