f83160e8beb0ec9eafc1eba0a0765fa61eaf1ad1
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-03-12 01:42:44 +0530

    accreditations basic added

---
+++ src/app/components/accreditations/accreditations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accreditations',
+  templateUrl: './accreditations.component.html',
+  styleUrls: ['./accreditations.component.css']
+})
+export class AccreditationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
