938ac4aac52e810d151a4180cc839545882dd53b
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-03-13 00:37:53 +0530

    routing added for all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ProceduresComponent} from './components/procedures/procedures.component';
+import {DoctorsComponent} from './components/doctors/doctors.component';
+import {ChatRoomComponent} from './components/chat-room/chat-room.component';
+import {BookingComponent} from './components/booking/booking.component';
 
 const routes: Routes = [
   {
@@ -11,6 +15,22 @@ const routes: Routes = [
     path: '',
     component: DashboardComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'procedures',
+    component: ProceduresComponent
+  },
+  {
+    path: 'doctors',
+    component: DoctorsComponent
+  },
+  {
+    path: 'chatroom',
+    component: ChatRoomComponent
+  },
+  {
+    path: 'booking',
+    component: BookingComponent
   }
 ];
 
