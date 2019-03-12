159100e3ce5e5f8878a7a0892cb45a8a0d5ca94b
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-03-13 02:16:04 +0530

    headings added and dashboard organized

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  title = 'Durdans Hospital PLC';
+  address = 'Singapore, 12 Woodbine long named road';
+  titleEditing = false;
+  addressEditing = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeTitle(title: String) {
+    this.title = title;
+    this.titleEditing = false;
+  }
+
+  editTitle(): void {
+    this.titleEditing = true;
+  }
+
+  changeAddress(address: String) {
+    this.address = address;
+    this.addressEditing = false;
+  }
+
+  editAddress(): void {
+    this.addressEditing = true;
+  }
 }
