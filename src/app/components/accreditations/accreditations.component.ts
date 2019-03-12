6fbb4423696d2e862496181971aad82106aa919d
kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2019-03-12 15:29:00 +0530

    delete and add functionality added for accreditations some other small changes

--- src/app/components/accreditations/accreditations.component.ts
+++ src/app/components/accreditations/accreditations.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccreditationsComponent implements OnInit {
 
+  accreditations = [1, 2, 3, 4, 5, 6];
   constructor() { }
 
   ngOnInit() {
   }
 
+  removeAccreditation(i: number): void {
+    this.accreditations.splice(i, 1);
+  }
+
+  addAccreditation(): void {
+    this.accreditations.push(1);
+  }
 }
